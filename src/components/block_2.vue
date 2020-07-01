4b2d7cc9b437e5058b9a4daf4d025ac5a19721e3
mouseinyl <rmaaron11@gmail.com>
2020-07-01 14:20:45 -0400

    correcion de tamañon en meedio

--- src/components/block_2.vue
+++ src/components/block_2.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row">
     <!--  -->
-    <div class="col s12">
+    <div class="col l12">
       <div class>
         <h5 class="alegreya marron_text center">Para amanates del cafe</h5>
-        <div class="col s8 offset-s2">
+        <div class="col m10 l8 offset-l2 offset-m1">
           <div class="col s3 conten_img">
             <div class="col s12 circulo">
               <img class="col s12" src="@/assets/select.svg" alt />
@@ -32,7 +32,7 @@
     <!--  -->
     <div class="col s12 margin_top cubito">
       <!--  -->
-      <div class="col s8 offset-s2 margin_top_10">
+      <div class="col m10 l8 offset-l2 offset-m1 margin_top_10">
         <h5 class="alegreya white-text center">Para amanates de la lectura</h5>
 
         <div class="col s3 conten_img">
@@ -91,5 +91,7 @@ export default {
 .margin_top_10 {
   margin-top: 15%;
   margin-bottom: 8%;
+}
+@media only screen and (min-width: 600px) and (max-width: 992px) {
 }
 </style>
