0efed223cb8cf761aaee9f5ae11981d6c02c4219
mouseinyl <rmaaron11@gmail.com>
2020-07-03 08:58:42 -0400

    ruta /view/information agreagada

--- src/components/Navigation_bar.vue
+++ src/components/Navigation_bar.vue
@@ -2,27 +2,25 @@
   <div class="cou-fixed">
     <div class="row">
       <div class="col s12 smoke bar valign-wrapper marron_text">
-        <div class="col s3  center">
-          <router-link to="/Info"
-            ><i class="material-icons marron_text">info</i></router-link
-          >
+        <div class="col s3 center">
+          <router-link to="/Information">
+            <i class="material-icons marron_text">info</i>
+          </router-link>
         </div>
         <div class="col s3 center">
-          <router-link to="/"
-            ><i class="material-icons marron_text">home</i></router-link
-          >
+          <router-link to="/">
+            <i class="material-icons marron_text">home</i>
+          </router-link>
         </div>
         <div class="col s3 center">
           <router-link to="/Productos">
-            <img src="../assets/carrito-de-compras.svg" style="width:25%"
-          /></router-link>
+            <img src="../assets/carrito-de-compras.svg" style="width:25%" />
+          </router-link>
         </div>
         <div class="col s3 center">
-          <router-link to="/Login"
-            ><i class="material-icons marron_text"
-              >account_circle</i
-            ></router-link
-          >
+          <router-link to="/Login">
+            <i class="material-icons marron_text">account_circle</i>
+          </router-link>
         </div>
       </div>
     </div>
@@ -30,7 +28,7 @@
 </template>
 <script>
 export default {
-  name: "Navigation_bar",
+  name: "Navigation_bar"
 };
 </script>
 <style scoped>
