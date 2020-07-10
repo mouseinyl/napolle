8725a33c927bf3736f5b6f601e8d5a282f095a2f
mouseinyl <rmaaron11@gmail.com>
2020-07-10 13:29:48 -0400

    correcion de componente *componente unico "banner" agregado

---
+++ src/components/Compartidos/Banner/TheBanner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="banner">
+    <banner-contenido :color="banner_white" :scroll="true" />
+  </div>
+</template>
+
+
+
+
+<script>
+import BannerContenido from "./BannerContenido.vue";
+
+export default {
+  name: "TheBanner",
+  components: {
+    BannerContenido
+  },
+
+  data: function() {
+    return {
+      initied: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
