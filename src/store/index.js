301e963914a8a2ceebfc86c7bc74e96fa0aaea7a
mouseinyl <rmaaron11@gmail.com>
2020-08-22 21:03:44 -0400

    actulizacion a metodologia de modulos segun vueex

--- src/store/index.js
+++ src/store/index.js
@@ -1,63 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import generalM from "./generales";
+import databaseM from "./database";
+
 Vue.use(Vuex);
-////////////////////////////////////////////////////////////////////////
-
-import {
-  db,
-  items,
-  auth
-} from "./firebase.js"
-
-/////////////////////////////////////////////////////////////////////////
 
 export default new Vuex.Store({
-  state: {
-    items: [],
-    selecion: [],
-    espera: false,
-    signin: false
-  },
-  mutations: {
-    cargando(state, dato) {
-      state.items = dato
+    modules: {
+        generalM,
+        databaseM
     },
-    estado(state, estado) {
-      state.espera = estado
-    },
-    onsignin(state, estado) {
-      console.log("aqui")
-      state.signin = estado;
-    },
-    aumentar(state, index) {
-      var pan = state.items[index];
-      if (pan.cantidad < pan.existencia) {
-        return pan.cantidad++;
-      }
-    },
-    disminuir(state, index) {
-      var pan = state.items[index];
-      if (pan.cantidad > 0) {
-        return pan.cantidad--;
-      }
-    },
-    valor_por_cantidad(state, index) {
-      var pan = state.items[index];
-      return pan.cantidad * pan.price;
-    },
-
-  },
-  actions: {
-    recuperando({
-      commit
-    }) { //recuperandos datos lista de productos
-      commit("estado", true)
-      items.once('value', snapshot => {
-        const documents = snapshot.val();
-        commit("cargando", documents);
-        commit("estado", false)
-      })
-    }
-  },
-  modules: {},
 });
