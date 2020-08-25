366fb470a58fc57dbabdd0ec71e2bdffc5e0a28f
mouseinyl <rmaaron11@gmail.com>
2020-08-25 11:45:07 -0400

    correciones generales *correccion de la referencia  a la base de datos

--- src/store/database.js
+++ src/store/database.js
@@ -1,33 +1,31 @@
-import {db} from "./firebase"
+import { db } from "./firebase"
 import generalM from "./generales";
-const state={
-    items:[],
-    temp_list_shop:[]
-    
+const state = {
+    items: [],
+    temp_list_shop: []
+
 }
-const mutations={
-    cargarItems(state,snapshop){
-        state.items=snapshop;
+const mutations = {
+    cargarItems(state, snapshop) {
+        state.items = snapshop;
     },
-    cargar_list_temp(state,temp){
-        console.log("exito")
-        state.temp_list_shop=temp
-        console.log("exito", state.temp_list_shop)
+    cargar_list_temp(state, temp) {
+        state.temp_list_shop = temp
     }
 }
-const actions={
-    getItems({commit}){
-       db.ref('items').on('value',(snap)=>{
-            const doc=snap.val()
-            commit('cargarItems',doc)
+const actions = {
+    getItems({ commit }) {
+        db.ref('items/productos').on('value', (snap) => {
+            const doc = snap.val()
+            commit('cargarItems', doc)
             generalM.mutations.Isloadding()
         })
     },
 }
 
- export default{
-    namespaced:true,
+export default {
+    namespaced: true,
     state,
     mutations,
     actions
- }
+}
