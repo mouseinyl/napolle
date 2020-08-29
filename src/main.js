e02554744797089a6702cf6d023d641284fab88a
mouseinyl <rmaaron11@gmail.com>
2020-08-28 20:13:58 -0400

    add importancion de depencias de mateiralize

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+import M from "materialize-css";
+
+
 
 Vue.config.productionTip = false
 import vuefire from 'vuefire'
 new Vue({
-  router,
-  store,
-  vuefire,
-  render: h => h(App)
+    router,
+    store,
+    vuefire,
+    M,
+    render: h => h(App)
 }).$mount('#app')
