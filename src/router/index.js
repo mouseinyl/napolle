ef87058e4a65e13bc1782c7a5e2685594ed95cb8
mouseinyl <rmaaron11@gmail.com>
2020-08-28 20:21:12 -0400

    added *agregado caracteristicas de routas {metas y nombre de pestana} *agrengado guardian de routa

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,94 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "../store";
+
+
 
 Vue.use(VueRouter);
 
 const routes = [{
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/Productos",
-    name: "Productos",
-    component: () =>
-      import( /* webpackChunkName: "Productos" */ "../views/Productos.vue"),
-  },
-  {
-    path: "/Cart",
-    name: "Cart",
-    component: () => import( /* webpackChunkName: "Cart" */ "../views/Cart.vue"),
+        path: "/",
+        name: "Home",
+        component: Home,
+        meta: {
+            name: "Home",
+            need_auth: false
+        }
+    },
+    {
+        path: "/Productos",
+        name: "Productos",
+        component: () =>
+            import ( /* webpackChunkName: "Productos" */ "../views/Productos.vue"),
+        meta: {
+            name: "Productos",
+            need_auth: false
+        }
+    },
+    {
+        path: "/Cart",
+        name: "Cart",
+        component: () =>
+            import ( /* webpackChunkName: "Cart" */ "../views/Cart.vue"),
+        meta: {
+            name: "Cart",
+            need_auth: true
+        }
 
-  },
-  {
-    path: "/Login",
-    name: "Login",
-    component: () =>
-      import( /* webpackChunkName: "Login" */ "../views/Login.vue"),
-  },
-  {
-    path: "/Registro",
-    name: "Registro",
-    component: () =>
-      import( /* webpackChunkName: "Login" */ "../views/Registro.vue"),
-  },
-  {
-    path: "/Perfil",
-    name: "Perfil",
-    component: () =>
-      import( /* webpackChunkName: "Login" */ "../views/Perfil.vue"),
-  },
-  {
-    path: "/Information",
-    name: "Information",
-    component: () =>
-      import( /* webpackChunkName: "Login" */ "../views/Information.vue"),
-  },
+    },
+    {
+        path: "/Login",
+        name: "Login",
+        component: () =>
+            import ( /* webpackChunkName: "Login" */ "../views/Login.vue"),
+        meta: {
+            name: "Login",
+            need_auth: false
+        }
+    },
+    {
+        path: "/Registro",
+        name: "Registro",
+        component: () =>
+            import ( /* webpackChunkName: "Login" */ "../views/Registro.vue"),
+        meta: {
+            name: "Registro",
+            need_auth: false
+        }
+    },
+    {
+        path: "/Perfil",
+        name: "Perfil",
+        component: () =>
+            import ( /* webpackChunkName: "Login" */ "../views/Perfil.vue"),
+        meta: {
+            name: "perfil",
+            need_auth: true
+        }
+    },
+    {
+        path: "/Information",
+        name: "Information",
+        component: () =>
+            import ( /* webpackChunkName: "Login" */ "../views/Information.vue"),
+        meta: {
+            name: "About",
+            need_auth: false
+        }
+    },
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
+
+router.beforeEach((to, from, next) => {
+    console.log()
+    if (to.meta.need_auth == true && store.state.authM.Is_logged == false) { next({ name: "Login" }) } else { next() }
+    window.document.title = to.name
+})
 
 export default router;
